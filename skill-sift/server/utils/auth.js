23e874a963fbd619bd61a2ba8bff619bd4953af8
taptapclicky <njhonny99@mail.com>
2023-08-01 21:22:09 -0400

    working on sign up and authentication

---
+++ skill-sift/server/utils/auth.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+const secret = 'supersecretpassword';
+const experation = '1h';
+
+module.exports = {
+    authMiddleware: function ({ req }) {
+        
+    }
+}
