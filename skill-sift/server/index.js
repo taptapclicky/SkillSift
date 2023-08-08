47e44d6d239468ddded41901270c9ab7068e1525
Jack Smith <jack_smith21554@yahoo.com>
2023-08-07 21:27:18 -0400

    Refactored server index.js file to include typeDefs and resolvers. Created index.js file in schemas folder to export resolvers and typedefs.

--- skill-sift/server/index.js
+++ skill-sift/server/index.js
@@ -1,17 +1,42 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
-    });
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+const startApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        })
+    })
+};
+
+startApolloServer();
+
+
 
